5a25f0d92b389c4b2174129a8c70fd432f1d3e97
Anant Kamath <kamathanant@gmail.com>
2012-04-03 15:40:19 +0530

    -Added dist-utils

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+ 
+pyATASmart = Extension('pyATASmart', 
+			sources = ['pyATASmart.c']
+                        include_dirs = ['/usr/local/include'],
+                        libraries = ['atasmart'],
+                        library_dirs = ['/usr/local/lib'],)
+ 
+setup (name = 'pyATASmart',
+       version = '0.1',
+       author='Anant Kamath',
+       author_email='kamathanant@gmail.com',
+       url='http://www.github.com/anantkamath/pyATASmart',
+
+       description = 'Python bindings for the libatasmart library',
+       ext_modules = [pyATASmart])
